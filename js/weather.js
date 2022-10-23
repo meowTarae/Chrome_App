86f6fc98115e4877ef2582ab4c0e672269301322
타래 <85001878+meowTarae@users.noreply.github.com>
2022-10-23 15:10:46 +0900

    make quote, finish

--- js/weather.js
+++ js/weather.js
@@ -4,14 +4,13 @@ const city = document.querySelector("#weather span:last-child");
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  console.log(lat, lon);
-  //   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
-  //   fetch(url)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       city.innerText = data.name;
-  //       weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
-  //     });
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 function onGeoError() {
   alert("Can't find you. No weather for you.");
